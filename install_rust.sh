7460907ed3442abe4936da06e25d674971dcc65f
Stevan Lohja <stevan.lohja>
2024-01-19 05:21:45 -0500

    fix: update rust install

--- install_rust.sh
+++ install_rust.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# Install Rust and Cargo
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-# Add Rust to PATH
-source $HOME/.cargo/env
+# Check if Rust and Cargo are installed
+if command -v rustc &> /dev/null && command -v cargo &> /dev/null; then
+    echo "Rust and Cargo are already installed."
+else
+    # Install Rust and Cargo
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    source $HOME/.cargo/env
+    echo "Rust and Cargo have been installed."
+fi
 
 # Verify installation
 rustc --version
 cargo --version
-
-
 # curl -o- https://raw.githubusercontent.com/stevanlohja/mn_useful_remote_scripts/main/install_rust.sh | bash
+# curl -o- https://raw.githubusercontent.com/stevanlohja/mn_useful_remote_scripts/main/install_rust.sh | bash -s
+# curl -o- https://raw.githubusercontent.com/stevanlohja/mn_useful_remote_scripts/main/install_rust.sh | source /dev/stdin
