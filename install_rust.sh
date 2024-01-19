0c2ddd19f442bce7d26c66db855188ad2355088c
Stevan Lohja <stevan.lohja>
2024-01-19 05:18:02 -0500

    fix: basic install for rust

--- install_rust.sh
+++ install_rust.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-# Check if Rust and Cargo are installed
-if command -v rustc &> /dev/null && command -v cargo &> /dev/null; then
-    echo "Rust and Cargo are already installed."
-else
-    # Prompt user for installation confirmation
-    read -p "Rust and Cargo are not found. Do you want to install them? (y/n): " choice
+# Install Rust and Cargo
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-    if [ "$choice" == "y" ]; then
-        # Install Rust and Cargo
-        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-        source $HOME/.cargo/env
-        echo "Rust and Cargo have been installed."
-    else
-        echo "Installation aborted. Rust and Cargo are not installed."
-    fi
-fi
+# Add Rust to PATH
+source $HOME/.cargo/env
 
 # Verify installation
 rustc --version
 cargo --version
+
+
+# curl -o- https://raw.githubusercontent.com/stevanlohja/mn_useful_remote_scripts/main/install_rust.sh | bash
